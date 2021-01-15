d429039c1762aed9baf5f32b25fdde8b26c6acd3
Ahmed Dawoud <adawoud1000@hotmail.com>
2021-01-15 03:06:11 +0200

    Movable Paddles

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pygame.locals import *
 WINDOWS_WIDTH = 1280
 WINDOWS_HEIGHT = 720
 
-PADDLE_SPEED = 1600
+PADDLE_SPEED = 800
 
 pygame.init()
 smallFont = pygame.font.Font('font.TTF', 27)
@@ -19,6 +19,14 @@ player2Score = 0
 player1Y = 30
 player2Y = WINDOWS_HEIGHT - 120
 
+pressed_S = False
+pressed_W = False
+
+pressed_UP = False
+pressed_DOWN = False
+
+clock = pygame.time.Clock()
+
 
 class Pong():
     def __init__(self):
@@ -27,8 +35,8 @@ class Pong():
         pygame.display.set_caption('Pong')
 
         # Player 1
-        pygame.draw.rect(windowSurface, (255, 255, 255),
-                         pygame.Rect(15, player1Y, 15, 80))
+        Player1 = pygame.draw.rect(windowSurface, (255, 255, 255),
+                                   pygame.Rect(15, player1Y, 15, 80))
 
         # Player 2
         pygame.draw.rect(windowSurface, (255, 255, 255),
@@ -64,17 +72,53 @@ class Pong():
         pygame.display.update()
 
     def play(self):
+        global player1Y
+        global player2Y
+
+        global pressed_S
+        global pressed_W
+        global pressed_UP
+        global pressed_DOWN
+
+        dt = 1/clock.tick(30)
+        # print(dt)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            if event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     exit()
+                elif event.key == pygame.K_s:
+                    pressed_S = True
+                elif event.key == pygame.K_w:
+                    pressed_W = True
+                elif event.key == pygame.K_UP:
+                    pressed_UP = True
+                elif event.key == pygame.K_DOWN:
+                    pressed_DOWN = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_s:
+                    pressed_S = False
+                elif event.key == pygame.K_w:
+                    pressed_W = False
+                elif event.key == pygame.K_UP:
+                    pressed_UP = False
+                elif event.key == pygame.K_DOWN:
+                    pressed_DOWN = False
+
+        if pressed_S:
+            player1Y += PADDLE_SPEED*dt
+        elif pressed_W:
+            player1Y -= PADDLE_SPEED*dt
+        if pressed_DOWN:
+            player2Y += PADDLE_SPEED*dt
+        elif pressed_UP:
+            player2Y -= PADDLE_SPEED*dt
 
 
-game = Pong()
 while True:
+    game = Pong()
     play = game.play()
 
     # for event in pygame.event.get():
