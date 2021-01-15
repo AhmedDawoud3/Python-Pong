1d14b043000dc4362a94f2552e95981a0641460b
Ahmed Dawoud <adawoud1000@hotmail.com>
2021-01-15 02:11:50 +0200

    Add The Scores

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ from pygame.locals import *
 WINDOWS_WIDTH = 1280
 WINDOWS_HEIGHT = 720
 
+PADDLE_SPEED = 1600
+
 pygame.init()
-font = pygame.font.Font('font.TTF', 27)
+smallFont = pygame.font.Font('font.TTF', 27)
+
+scoreFont = pygame.font.Font('font.TTF', 108)
+
+player1Score = 0
+player2Score = 0
+
+player1Y = 30
+player2Y = WINDOWS_HEIGHT - 120
 
 
 class Pong():
@@ -16,22 +26,38 @@ class Pong():
             (WINDOWS_WIDTH, WINDOWS_HEIGHT))
         pygame.display.set_caption('Pong')
 
-        text = font.render('Hello Pong!', True, (255, 255, 255), 0)
-        textRect = text.get_rect()
-        textRect.centerx = windowSurface.get_rect().centerx
-        textRect.centery = windowSurface.get_rect().centery - 250
-
         # Player 1
         pygame.draw.rect(windowSurface, (255, 255, 255),
-                         pygame.Rect(15, 60, 15, 80))
+                         pygame.Rect(15, player1Y, 15, 80))
 
         # Player 2
         pygame.draw.rect(windowSurface, (255, 255, 255),
-                         pygame.Rect(1250, 600, 15, 80))
+                         pygame.Rect(1250, player2Y, 15, 80))
 
         # Ball
         pygame.draw.rect(windowSurface, (255, 255, 255),
                          pygame.Rect(WINDOWS_WIDTH/2 - 3.5, WINDOWS_HEIGHT / 2 - 3.5, 12, 12))
+
+        text = smallFont.render('Hello Pong!', True, (255, 255, 255), 0)
+        textRect = text.get_rect()
+        textRect.centerx = windowSurface.get_rect().centerx
+        textRect.centery = windowSurface.get_rect().centery - 250
+
+        windowSurface.blit(text, textRect)
+
+        # Player One Score
+        text = scoreFont.render(str(player1Score), True, (255, 255, 255), 0)
+        textRect = text.get_rect()
+        textRect.centerx = windowSurface.get_rect().centerx - 120
+        textRect.centery = windowSurface.get_rect().centery - 120
+
+        windowSurface.blit(text, textRect)
+
+        # Player Two Score
+        text = scoreFont.render(str(player2Score), True, (255, 255, 255), 0)
+        textRect = text.get_rect()
+        textRect.centerx = windowSurface.get_rect().centerx + 120
+        textRect.centery = windowSurface.get_rect().centery - 120
 
         windowSurface.blit(text, textRect)
 
